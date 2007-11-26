af64d89058cf35522782c2c0493b66a652c13898
Eivind Uggedal <eu@redflavor.com>
2007-11-26 16:36:55 +0100

    Seperated scm files.

--- typeraker/lib/typeraker/scm.rb
+++ typeraker/lib/typeraker/scm.rb
@@ -23,60 +23,8 @@ module Typeraker
       end
     end
 
-    class Mercurial < Base
-
-      def initialize
-        super
-
-        @name = 'Mercurial'
-        disable_stdout do
-          disable_stderr do
-            @executable = 'hg' if system 'which hg'
-          end
-        end
-
-        @revision, @date = parse_scm_stats
-
-        yield self if block_given?
-      end
-
-      def parse_scm_stats
-        return [nil, nil] unless @executable
-
-        raw_stats = `hg tip`
-        revision = raw_stats.scan(/^changeset: +(.+)/).first.first
-        date = raw_stats.scan(/^date: +(.+)/).first.first
-
-        [revision, date]
-      end
-    end
-
-    class Subversion < Base
-
-      def initialize
-        super
-
-        @name = 'Subversion'
-        disable_stdout do
-          disable_stderr do
-            @executable = 'svn' if system 'which svn'
-          end
-        end
-
-        @revision, @date = parse_scm_stats
-
-        yield self if block_given?
-      end
-
-      def parse_scm_stats
-        return [nil, nil] unless @executable
-
-        raw_stats = `svn info`
-        revision = raw_stats.scan(/^Revision: (\d+)/).first.first
-        date = raw_stats.scan(/^Last Changed Date: (.+)/).first.first
-
-        [revision, date]
-      end
+    %w(mercurial subversion).each do
+      |f| require File.dirname(__FILE__) + "/scm/#{f}"
     end
   end
 end
