6596d6de6d69c8aed58d5bd9d1d909afcda30a0a
Eivind Uggedal <eu@redflavor.com>
2008-01-31 16:24:37 +0100

    Fixed file path bug.

--- typeraker/lib/typeraker/spell.rb
+++ typeraker/lib/typeraker/spell.rb
@@ -4,10 +4,8 @@ module Typeraker
       source_files = FileList["#{Typeraker.options[:source_dir]}/*.tex"]
       dictionary_path = File.join(Typeraker.options[:spell_dir],
                                   Typeraker.options[:spell_file])
-      puts dictionary_path
       source_files.each do |file|
-        file_path = File.join(Typeraker.options[:source_dir], file)
-        system "ispell -t -x -p #{dictionary_path} #{file_path}"
+        system "ispell -t -x -p #{dictionary_path} #{file}"
       end
     end
   end
